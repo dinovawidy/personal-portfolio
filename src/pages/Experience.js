5dcd413ef84e4b7b27c462ee655517ab7ba6f020
dinova w <dinovaprogram@gmail.com>
2022-07-29 02:56:48 +0700

    second commit

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
+import WorkIcon from "@material-ui/icons/Work";
+
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <div className="experience">
+      <VerticalTimeline lineColor="#010003">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2014 - 2017"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Elementary School State 1 Pacitan, East Java, Indonesia
+          </h3>
+          <p> High School Diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2017 - 2022"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Adhi Tama Institut of Technology Surabaya, East Java, Indonesia
+          </h3>
+          <p> Bachelor Degree </p>
+          <h4 className="vertical-timeline-element-subtitle">Computer Science</h4>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2021"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Web Developer - Asswell Laundry
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+          Sidoarjo, Indonesia
+          </h4>
+          <p> Developed Point of Sales </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2022"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          SEO Content Specialist - AsiaCommerce Network 
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+          Surabaya, Indonesia
+          </h4>
+          <p> adbashjdbahjdbahb </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
+};
 
-export default Experience
+export default Experience;
